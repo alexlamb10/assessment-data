92e70e654217a82a76be62de71012e9d11a5c6be
Alex Lamb <alexlamb104@gmail.com>
2022-03-11 11:57:47 -0700

    Completed last Extra Credit, printed all orders > 10 in python

--- process.py
+++ process.py
@@ -15,4 +15,25 @@ def sales_reports(log_file):
             print(line)
 
 #invoke the function passing in log_file
-sales_reports(log_file)
+# sales_reports(log_file)
+
+
+list_of_deliveries = []
+
+
+def list_to_string(list):
+    str1 = " "
+    return (str1.join(list))
+
+
+def print_list_greater_than_ten(log_file):
+    for line in log_file:
+        line = line.rstrip('\n').split(' ')
+        list_of_deliveries.append(line)
+
+    for item in list_of_deliveries:
+        melon_count = int(item[2])
+        if melon_count > 10:
+            print(list_to_string(item))
+
+print_list_greater_than_ten(log_file)
